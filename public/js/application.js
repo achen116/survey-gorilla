de7b646b9660822474f23a5690709694a60c14e8
cusackalex <cusackalex@gmail.com>
2015-06-19 12:29:53 -0700

    ajaxing to save answer works

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('.answer_submit').on("click", function(event){
+    event.preventDefault();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    var item = $('form').serialize();
+    console.log(item)
+    $.ajax({
+      url: $('form').attr("action"),
+      method: "POST",
+      data: {content: item},
+      dataType: 'JSON'
+
+    }).done(function(data){
+      console.log(data)
+
+    })
+  })
+
 });
