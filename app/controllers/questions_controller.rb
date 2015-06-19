256231c97c0da57d9f969b59ec663a0ddf2b4201
Arielle Chen & Jeff Chang <pair+achen116.jchang2014@devbootcamp.com>
2015-06-18 17:52:27 -0700

    almost ready mvp. need to implement sessions and take survey

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,18 @@
 get '/surveys/:id/questions/new' do
-  # @survey = Survey.find(params[:id])
+  @survey = Survey.find(params[:id])
+  p @survey
   erb :question_answer_form
 end
 
 post '/surveys/:id/questions' do
-  # question = Question.new(question: params[:question], survey_id: params[:id])
+  @survey = Survey.find(params[:id])
+  question = Question.new(question: params[:question], survey_id: params[:id])
 
   if question.save
-    # answer1 = Answer.new(answer: params[:answer1], question_id: question.id)
-    # answer2 = Answer.new(answer: params[:answer2], question_id: question.id)
-    # answer3 = Answer.new(answer: params[:answer3], question_id: question.id)
+    answer1 = QuestionAnswer.new(the_answer: params[:answer1], question_id: question.id)
+    answer2 = QuestionAnswer.new(the_answer: params[:answer2], question_id: question.id)
+    answer3 = QuestionAnswer.new(the_answer: params[:answer3], question_id: question.id)
+
     answers = [answer1, answer2, answer3]
     answers.each do |answer|
       if !answer.save
@@ -17,7 +20,7 @@ post '/surveys/:id/questions' do
         erb :question_answer_form
       end
     end
-    redirect "/surveys/:id"
+    redirect "/surveys/#{@survey.id}"
   else
     @error = "Invalid Question!"
     erb :question_answer_form
